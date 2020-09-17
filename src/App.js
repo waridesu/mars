21c0c93db59ca92ba205075d1f637058de7cc410
Denis <waridesu@gmail.com>
2020-09-18 01:10:49 +0300

    somelogic

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ const App = () => {
     const [sol, setSol] = useState(null)
     const [data, setData] = useState(null)
     let [imageToShow, setImagetoShow] = useState(2);
+    const [init, setInit] = useState(false)
     const hendleChange1 = (e) => {
         setRover(e.currentTarget.value)
-        setImagetoShow(imageToShow=2)
+        setImagetoShow(imageToShow = 2)
     }
     const hendleChange2 = (e) => {
         setCamera(e.currentTarget.value)
-        setImagetoShow(imageToShow=2)
+        setImagetoShow(imageToShow = 2)
     }
     const hendleChange3 = (e) => {
         setSol(e.currentTarget.value)
-        setImagetoShow(imageToShow=2)
+        setImagetoShow(imageToShow = 2)
     }
     const hendleSubmit = (e) => {
         axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&api_key=CMmKidrraZtZG62lBQz4zbQYziw1sAKfufDdZBC8`)
@@ -26,6 +27,7 @@ const App = () => {
                 setData(rez.data.photos.slice(0, imageToShow))
             })
         e.preventDefault()
+        setInit(true)
     }
     const loadMore = () => {
         setImagetoShow(imageToShow + 2)
@@ -35,8 +37,7 @@ const App = () => {
             .then((rez) => {
                 setData(rez.data.photos.slice(0, imageToShow))
             })
-    }, [imageToShow])
-
+    }, [imageToShow, data])
     return <div className="App">
         <form onSubmit={hendleSubmit}>
             <p>Rover</p>
@@ -52,14 +53,15 @@ const App = () => {
                 <option value={"navcam"}>Navigation</option>
             </select>
             <div>
-                <label>Sol</label>
+                <label>Sol<br/></label>
                 <input onChange={hendleChange3}/>
             </div>
             <button>let's see</button>
         </form>
         <>
-            {!data ? null : data.map((d) => <><img key={d.id} src={d.img_src} alt={'mars'}/></>)}
-            {!data ? null : <button onClick={loadMore}>showMore</button>}
+            {data && data.map((d) => <div className="imageContainer"><img key={d.id} src={d.img_src} alt={'mars'}/>
+            </div>)}
+            {init && data !== [] && <button onClick={loadMore}>showMore</button>}
         </>
     </div>
 }
